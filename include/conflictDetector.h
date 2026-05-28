3edb55609f842f12ef582413dff4d10b36b1a3fc
Admin <uongvanthinh2007@gmail.com>
2026-05-28 14:08:06 +0700

    hahaha

---
+++ include/conflictDetector.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include"commonTypes.h"
+#include<unordered_map>
+#include<vector>
+#include<string>
+
+bool kiemTraTrungGio(lopHoc& lop, std::vector<lopHoc>& lichHienTai);
+bool kiemTraDieuKienLoc(lopHoc& lop, dieuKienLoc& dieuKien);
--- src/scheduleEngine.cpp
+++ src/scheduleEngine.cpp
@@ -2,5 +2,5 @@
 #include"../include/schedule.h"
 
 void xepLich(){
-
+    
 };

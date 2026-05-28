86fd960010a0d6358b89d334196b2da8e8bb2adb
MTriiN <minhtridz777@gmail.com>
2026-05-28 13:41:23 +0700

    minh tri dep trai

--- include/dataParser.h
+++ include/dataParser.h
@@ -1,8 +1,9 @@
+#pragma once
+
 #include "commonTypes.h"
 #include <string>
 #include <vector>
 #include <unordered_map>
 
 std::unordered_map<std::string,std::vector<lopHoc>> docFileCSV(std::string duongDan);
-int chuyenThuSangSo(std::string thu);
 std::vector<int> phanTichTuanHoc(std::string tuan);
